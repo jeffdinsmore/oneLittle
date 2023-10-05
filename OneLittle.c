9f100aa2201e15943c3465a0e600953c7b40d529
jeffdinsmore <hello@jeffdinsmore.com>
2023-10-05 15:00:03 -0700

    update program to add c function

--- OneLittle.c
+++ OneLittle.c
@@ -10,16 +10,32 @@
 #define NITEMS 10
 
 char thing[] = "spider";
-
+//char things = "";
 int main() {
-    for(int i = 1; i <= NITEMS; i++){
+	
+	void displayCountUp(int count, char thing[], int MaxCount){
+		if(count % 3 == 0)
+            printf("%i little %ss\n", count, thing);
+        else if(count == MaxCount)
+            printf("%i little %s bugs\n", count, thing);
+        else
+            printf("%i little\n", count);
+	}
+	void print(char things[]){
+		printf("%s", things);
+	}
+		for(int i = 1; i <= NITEMS; i++){
+			displayCountUp(i, thing, NITEMS);
+		}
+		/*for(int i = 1; i <= NITEMS; i++){
+			//displayCount(i, NITEMS)
         if(i % 3 == 0)
             printf("%i little %ss\n", i, thing);
         else if(i == NITEMS)
             printf("%i little %s bugs\n", i, thing);
         else
             printf("%i little\n", i);
-    }
+    }*/
     printf("\n");
     int j = 9;
     for(int i = NITEMS; i >= 1; i--){
@@ -42,6 +58,6 @@ int main() {
         else
             printf("%i little, ", i);
     }
-    
+    print("Happy");
     return 0;
 }
