70c0380fcb84b9ce2fdde832ccbc04d02ba5c91e
jeffdinsmore <hello@jeffdinsmore.com>
2023-10-06 16:43:58 -0700

    added a count down display

--- OneLittle.c
+++ OneLittle.c
@@ -9,7 +9,7 @@
 
 #define NITEMS 10
 
-char thing[] = "spider";
+char thing[] = "spidey";
 
 int main() {
 	void displayCountUp(int count, char thingy[], int MaxCount){
@@ -20,13 +20,22 @@ int main() {
         else
             printf("%i little, ", count);
 	}
-	void print(char things[]){
-		printf("%s", things);
+	void displayCountDown(int count, char thingy[], int MaxCount){
+		if((MaxCount+1-count) % 3 == 0)
+			printf("%i little %ss\n", count, thingy);
+		else if(count == 1)
+			printf("%i little %s bug\n", count, thingy);
+		else
+			printf("%i little, ", count);
 	}
 	for(int i = 1; i <= NITEMS; i++){
-		displayCountUp(i, "Spider", NITEMS);
+		displayCountUp(i, "spider", NITEMS);
 	}
     printf("\n");
+	for(int i = NITEMS; i >= 1; i--){
+		displayCountDown(i, "spider", NITEMS);
+	}
+	printf("\n");
     int j = 9;
     for(int i = NITEMS; i >= 1; i--){
         if((i-j) % 3 == 0)
@@ -38,16 +47,5 @@ int main() {
         j = j-2;
     }
     printf("\n");
-    int counter = 0;
-    for(int i = NITEMS; i >=1; i--){
-        counter = counter + 1;
-        if(counter % 3 == 0)
-            printf("%i little %ss\n", i, thing);
-        else if(i == 1)
-            printf("%i little %s bug\n", i, thing);
-        else
-            printf("%i little, ", i);
-    }
-    print("Happy");
     return 0;
 }
